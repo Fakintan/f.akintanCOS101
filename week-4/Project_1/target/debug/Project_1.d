9cb20e268dd0ee6a1fad3b36708a768ca5660f72
F.Akintan <faith.akintan@pau.edu.ng>
2026-09-18 21:08:19 +0100

    Project1

---
+++ week-4/Project_1/target/debug/Project_1.d
@@ -0,0 +1,1 @@
+C:\Users\NEW\Documents\f.akintanCOS101\week-4\Project_1\target\debug\Project_1.exe: C:\Users\NEW\Documents\f.akintanCOS101\week-4\Project_1\src\main.rs
---
+++ week-4/main.rs
@@ -0,0 +1,37 @@
+fn main() {
+    let mut input1 = String::new();
+    let mut input2 = String::new();
+    let mut input3 = String::new();
+
+    println!("Enter a: ");
+    io::stdin().read_line(&mut input1).expect("Not a valid string");
+    let a:f32 = input1.trim().parse().expect("Not a valid number");
+
+    println!("Enter b: ");
+    io::stdin().read_line(&mut input2).expect("Not a valid string");
+    let b:f32 = input2.trim().parse().expect("Not a valid number");
+
+    println!("Input c: ");
+    io::stdin().read_line(&mut input3).expect("Not a valid string");
+    let c:f32 = input3.trim().parse().expect("Not a valid number");
+
+    let d = b * b - 4.0 * a * c;
+
+    if d > 0.0 {
+        let x1 = (-b + d.sqrt()) / (2.0 * a);
+        let x1 = (-b - d.sqrt()) / (2.0 * a);
+
+        println!("TWo distinct roots: {}", x1, x2);
+    }
+    else if d == 0.0 {
+        let x = -b / (2.0 * a);
+
+        println!("One real root: {}", x );
+
+    }
+    else {
+        println!("No real roots");
+    }
+
+
+}
